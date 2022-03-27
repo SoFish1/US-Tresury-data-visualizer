4bccb2c5102e6c8dbd7c3da028ffd96e24be925c
SoFish1 <andrea.valentini.1993@gmail.com>
2022-03-27 17:29:59 +0200

    refactoring ongoing

---
+++ backend/api/db_load/routes.py
@@ -0,0 +1,20 @@
+
+from . import db_load_bp 
+
+# @db_load_bp.route("/token", methods=["POST"])
+# @validate(body=UserLogin)
+# def login():
+    
+#     email = request.body_params.email
+#     password = request.body_params.password
+
+#     user = User.query.filter_by(email=str(email)).first()
+    
+#     if user is None or not user.check_password(password):
+#         return BaseMessage(message="Bad username or password"), 401
+    
+#     elif user.email_confirmed_at is None :
+#         return BaseMessage(message="The account is not activated"), 401
+    
+#     access_token = create_access_token(identity=email)
+#     return jsonify(access_token=access_token)
--- backend/flaskr/db_load/__init__.py
+++
@@ -1,5 +0,0 @@
-# from flask import Blueprint
-
-# db_load_bp = Blueprint('auth', __name__, url_prefix='/backend/db_load')
-
-# from . import routes
